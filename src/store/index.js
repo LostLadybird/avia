ff52ccda10f764349035435ef61cd7ccd14ed1f0
Yuliya Zarichnyuk <julia_mg@rambler.ru>
2023-10-12 23:59:34 +0300

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import ticketReducer from './ticketSlice';
 
 export default configureStore({
   reducer: {
     tickets: ticketReducer,
   },
-  devTools: process.env.NODE_ENV !== 'production',
 });
